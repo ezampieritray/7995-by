08b42bbe5295a85ec1e95f71745602c2edb5041f
Vitaly Zdanevich <zdanevich.vitaly@ya.ru>
2015-10-09 17:38:25 +0300

    js email send

--- main.js
+++ main.js
@@ -53,3 +53,28 @@ function toggleVisabilityOfOneReview(classList) {
     classList.toggle('hidden');
     classList.toggle('shows');
 }
+
+function send() {
+    var model =   document.getElementById('model').value;
+    var problem = document.getElementById('problem').value;
+    var name =    document.getElementById('name').value;
+    var phone =   document.getElementById('phone').value;
+    
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (xhttp.readyState == 4 && xhttp.status == 200) {
+            console.log(xhttp.responseText);
+        }
+    }
+    xhttp.open('GET', 'https://mandrillapp.com/api/1.0/messages/send.json?message[from_email]=mail@7995.by&message[to][0][email]=zdanevich.vitaly@ya.ru&message[subject]=Заявка%20с%207995.by&message[html]=xxxxxx&key=oxdROOvCpKCp6InvVDqiGw', true);
+    xhttp.send();
+
+//     var form = document.createElement('form');
+//     form.setAttribute('method', 'POST');
+//     form.setAttribute('action', 'https://mandrillapp.com/api/1.0/messages/send.json');
+//     form.setAttribute('email', 'zdanevich.vitaly@ya.ru');
+//     form.setAttribute('subject', 'Заявка с 7995.by');
+//     form.setAttribute('html', 'This is the temp body');
+//     form.submit();
+
+}
