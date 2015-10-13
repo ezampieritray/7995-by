4b7a6e3a54c747e135316cb40f796fa72d1a18e0
Vitaly Zdanevich <zdanevich.vitaly@ya.ru>
2015-10-13 14:24:17 +0300

    modal

--- main.js
+++ main.js
@@ -62,19 +62,55 @@ function toggleVisabilityOfOneReview(classList) {
     classList.toggle('shows');
 }
 
-function send() {
+function handleForm() {
     var model =   document.getElementById('model').value;
     var problem = document.getElementById('problem').value;
     var name =    document.getElementById('name').value;
     var phone =   document.getElementById('phone').value;
-    
+
+    var regex = new RegExp('^\\+375 \\((17|29|33|44)\\) [0-9]{3}-[0-9]{2}-[0-9]{2}$');
+    if (regex.test(phone))
+        sendRequest(model, problem, name, phone);
+    else    
+        alert('Неверный формат телефона');
+}
+
+function isEnter(e) {
+    if (e.keyCode == 13) handleForm();
+}
+function sendRequest(model, problem, name, phone) {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
         if (xhttp.readyState == 4 && xhttp.status == 200) {
-            console.log(xhttp.responseText);
+            alert('Заявка отправлена, скоро мы вам позвоним');
         }
     }
     var message = 'Модель: ' + model + '<br>Что случилось: ' + problem + '<br>Имя: ' + name + '<br>Телефон: ' + phone;
     xhttp.open('GET', 'https://mandrillapp.com/api/1.0/messages/send.json?message[from_email]=mail@7995.by&message[to][0][email]=zdanevich.vitaly@ya.ru&message[subject]=Заявка%20с%207995.by&message[html]='+message+'&key=oxdROOvCpKCp6InvVDqiGw', true);
+    xhttp.send();
+}
+
+function showModal() {
+    if (document.getElementById('modal-send-review').style.display == 'none')
+        document.getElementById('modal-send-review').style.display = 'block';
+    else 
+        document.getElementById('modal-send-review').style.display = 'none';
+}
+
+document.onkeydown = handleKey;
+function handleKey(e) {
+    if (e.keyCode == 27) // esc
+        document.getElementById('modal-send-review').style.display = 'none';
+}
+
+function sendReview() {
+    var review = document.getElementById('users-review').value;
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (xhttp.readyState == 4 && xhttp.status == 200) {
+            alert('Ваш отзыв отправлен, спасибо, мы его изучим - нам важно чтобы вы были довольны')
+        }
+    }
+    xhttp.open('GET', 'https://mandrillapp.com/api/1.0/messages/send.json?message[from_email]=mail@7995.by&message[to][0][email]=zdanevich.vitaly@ya.ru&message[subject]=Отзыв%20с%207995.by&message[html]='+review+'&key=oxdROOvCpKCp6InvVDqiGw', true);
     xhttp.send();
 }
