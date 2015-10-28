199f591f17a8511f3ed5b397eafe515d14359ad2
Vitaly Zdanevich <zdanevich.vitaly@ya.ru>
2015-10-28 16:11:39 +0300

    address, phone, one-of-two skypes removed

--- main.js
+++ main.js
@@ -16,13 +16,19 @@ ymaps.ready(function() {
         });
     myMap.geoObjects
         .add(new ymaps.Placemark([53.941135, 27.599812], {
-            balloonContent: 'Минск Мележа 1 (офис 23)'
+            balloonContent: 'Минск Мележа 1 (БЦ Парус) офис 23'
         }, {
             preset: 'islands#circleIcon',
             iconColor: '#3caa3c'
         }))
         .add(new ymaps.Placemark([53.904671, 27.495526], {
             balloonContent: 'Минск Одоевского 28 офис 8'
+        }, {
+            preset: 'islands#circleIcon',
+            iconColor: '#3caa3c'
+        }))
+        .add(new ymaps.Placemark([53.88428, 27.448275], {
+            balloonContent: 'Минск Шаранговича 25 (ТЦ Магнит пав. 230)'
         }, {
             preset: 'islands#circleIcon',
             iconColor: '#3caa3c'
